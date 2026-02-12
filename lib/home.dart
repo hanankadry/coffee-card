b9bdedaedc60119ca0922bee65aa19a445368ddb
Hanan <hanan.kadry@outlook.com>
2026-02-12 21:49:03 +0200

    Adding body content for Home Page

--- lib/home.dart
+++ lib/home.dart
@@ -14,7 +14,27 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.brown[700],
         centerTitle: true,
       ),
-      body: const Text('Home content'),
+      body: Column(
+        children: [
+          Container(
+            color: Colors.brown[200],
+            padding: EdgeInsets.all(20),
+            child: const Text('How I like my coffee'),
+          ),
+          Container(
+            color: Colors.brown[100],
+            padding: EdgeInsets.all(20),
+            child: const Text('coffee preferences...'),
+          ),
+          Expanded(
+            child: Image.asset(
+              'assets/img/coffee_bg.jpg',
+              fit: BoxFit.fitWidth,
+              alignment: Alignment.bottomCenter,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
